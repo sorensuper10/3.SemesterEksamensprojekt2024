7f0f01ea521ce84f72158fa138d7f3b8ca3bccee
Diaco <diacosabir@hotmail.com>
2024-11-19 10:06:11 +0100

    1.1

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
+const path = require('path');
+
+const app = express()
+
+
+app.set('view engine', 'ejs');
+
+// Sæt views-mappen
+app.set('views', path.join(__dirname, 'views'));
+
+// Route til at renderere EJS-filen
+app.get('/', (req, res) => {
+    res.render('index'); // index.ejs i views-mappen
+});
+//Run server
+app.listen(port, () => {
+    console.log('Server is running at http://localhost:3000');
+});
