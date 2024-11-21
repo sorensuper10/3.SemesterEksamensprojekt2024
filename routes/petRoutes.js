a4ff7412c9a7059b1fd0cbab73e2c48a5e46d14a
amalieasborg <amalieasborg@gmail.com>
2024-11-21 11:21:17 +0100

    Middleware

--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const petController = require("../controllers/petController");
+const {validatePet} = require("../middlewares/validationMiddleware");
 const router = express.Router();
 
 router.get("/pets", petController.getAllPets);
 router.get('/pet', petController.getPetById);
 
 router.get('/createPet', petController.createPetEJS);
-router.post('/createPet', petController.createPet);
+router.post('/createPet', validatePet, petController.createPet);
 
-router.post('/pet/:id',petController.updatePet)
+router.post('/pet/:id', validatePet, petController.updatePet)
 
 router.post('/pet/:id',petController.deletePet)
 
