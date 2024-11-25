3894ec9f586a060d702f9fad98a3f488a714c69b
sorensuper10 <soren_super@hotmail.com>
2024-11-25 12:23:23 +0100

    Version 1.13

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const userController = require("../controllers/userController");
 const User = require("../models/userModel");
 
 const router = express.Router();
-const { checkRole } = userController;
+const {checkRole} = userController;
 
 // Login-side og handling
 router.get("/login", (req, res) => {
@@ -19,12 +19,14 @@ router.get("/create-user", (req, res) => {
 });
 router.post("/create-user", userController.postCreateUser);
 
+router.get("/deleteuser", userController.deleteUser);
+
 // Log ud
 router.get("/logout", userController.logout);
 
 // Admin-dashboard (kun for admins)
 router.get("/dashboardadmin", checkRole("admin"), (req, res) => {
-    res.render("dashboardadmin", { username: req.session.username });
+    res.render("dashboardadmin", {username: req.session.username});
 });
 
 // Rute til at vise formular for at redigere en bruger
@@ -35,7 +37,7 @@ router.get('/user/:id/edit', async (req, res) => {
             return res.status(404).send('Brugeren blev ikke fundet');
         }
         // Sender brugerdata til EJS
-        res.render('edit-user', { user });
+        res.render('edit-user', {user});
     } catch (err) {
         console.error(err);
         res.status(500).send("Fejl ved hentning af bruger til opdatering");
@@ -52,7 +54,7 @@ router.post('/user/:id/delete', userController.deleteUser);
 router.get("/all-users", checkRole("admin"), async (req, res) => {
     try {
         const users = await User.find();
-        res.render("all-users", { users });
+        res.render("all-users", {users});
     } catch (err) {
         console.error(err);
         res.status(500).send("Fejl under hentning af alle brugere");
@@ -66,7 +68,7 @@ router.get("/user/:id", async (req, res) => {
         if (!user) {
             return res.status(404).send("Brugeren blev ikke fundet");
         }
-        res.render("user-detail", { user });
+        res.render("user-detail", {user});
     } catch (err) {
         console.error(err);
         res.status(500).send("Fejl ved hentning af bruger");
