95a9f3aefada5d168a8532e87048166c7c70cc2e
sorensuper10 <soren_super@hotmail.com>
2024-11-26 12:17:55 +0100

    TDD

---
+++ tests/adoption.test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Adoption API', () => {
+    /**
+     * Test the GET route for all users
+     */
+    describe('GET /users', () => {
+        it('should GET all the users', (done) => {
+            chai.request(server)
+                .get('/allUsers')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.length.should.be.eql(5);
+                    done();
+                });
+        });
+    });
+});
